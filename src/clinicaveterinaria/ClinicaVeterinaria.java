b311c16d6053dea9ed5ade980f7bbc1cb278c62e
evsan <evsan@DESKTOP-71UFOPD>
2020-05-29 13:39:08 +0200

    Estos son los cambios realizados en las actividades A, B, C, D, E, F y G Este actualización pertenece al apartado H

--- src/clinicaveterinaria/ClinicaVeterinaria.java
+++ src/clinicaveterinaria/ClinicaVeterinaria.java
@@ -11,7 +11,12 @@ import java.io.InputStreamReader;
  */
 public class ClinicaVeterinaria {
 
-    /**
+    private static String nombre;
+	private static String apellidos;
+	private static String dni;
+	private static int edad;
+
+	/**
      * @param args the command line arguments
      */
     public static void main(String[] args) throws IOException {
@@ -38,12 +43,7 @@ public class ClinicaVeterinaria {
 
         //Tiene toda la pinta de ser un método
         do {
-            System.out.println("¿Qué quieres hacer?");
-            System.out.println("1. Añadir un nuevo cliente");
-            System.out.println("2. Añadir una nueva mascota para un cliente existente");
-            System.out.println("3. Mostrar todos los dueños");
-            System.out.println("4. Mostrar todas las mascotas");
-            System.out.println("5. Salir");
+            mostrarMenu();
             try {
                 numero = Integer.parseInt(teclado.readLine());
             } catch (IOException e) {
@@ -52,27 +52,10 @@ public class ClinicaVeterinaria {
                 System.err.println("Has causado una excepción porque no has entrado un número entero, gracias");
             }
 
-            //Otros métodos quizás?
+            //Otros m�todos quiz�s?
             switch (numero) {
                 case 1:
-                    //CLIENTE NUEVO
-                    System.out.println("INTRODUCIMOS EL CLIENTE");
-                    System.out.println("Introduce el nombre");
-                    String nombre = teclado.readLine();
-                    System.out.println("Introduce los apellidos");
-                    String apellidos = teclado.readLine();
-                    System.out.println("Introduce el DNI");
-                    String dni = teclado.readLine();
-                    System.out.println("Introduce la edad");
-                    int edad = 0;
-                    try {
-                        edad = Integer.parseInt(teclado.readLine());
-                    } catch (Exception e) {
-                        System.err.println("Has causado una excepción porque no has entrado un número entero, gracias");
-                        return;
-                    }
-                    Persona p1 = new Persona(nombre, apellidos, dni, edad);
-                    clientes.add(p1);
+				nuevoCliente(teclado, clientes);
                     break;
 
                 case 2:
@@ -94,51 +77,7 @@ public class ClinicaVeterinaria {
                         return;
                     }
 
-                    //MASCOTA NUEVA
-                    System.out.println("AHORA INTRODUCIMOS LA MASCOTA");
-                    System.out.println("Introduce el nombre");
-                    nombre = teclado.readLine();
-                    System.out.println("Introduce la raza");
-                    String raza = teclado.readLine();
-                    System.out.println("Introduce la edad");
-                    edad = 0;
-                    try {
-                        edad = Integer.parseInt(teclado.readLine());
-                    } catch (Exception e) {
-                        System.err.println("Has causado una excepción porque no has entrado un número entero, gracias");
-                        return;
-                    }
-
-                    int indiceM = 0;
-                    System.out.println("Introduce el tipo de mascota");
-                    System.out.println("1. Cerdo");
-                    try {
-                        indiceM = Integer.parseInt(teclado.readLine());
-                    } catch (IOException e) {
-                        System.err.println("Vaya, un error de entrada/salida... qué raro");
-                    } catch (NumberFormatException e) {
-                        System.err.println("Has causado una excepción porque no has entrado un número entero, gracias");
-                        return;
-                    }
-
-                    Mascota m1;
-                    switch (indiceM) {
-                        case 1:
-                            float peso = 0.0f;
-                            System.out.println("Introduce el peso del cerdo o la cerda");
-                            try {
-                                peso = Float.parseFloat(teclado.readLine());
-                            } catch (Exception e) {
-                                System.err.println("Has causado una excepción porque no has entrado un número entero, gracias");
-                                return;
-                            }
-                            m1 = new Cerdo(nombre, edad, raza, p2, peso);
-                            break;
-                        default:
-                            m1 = new Mascota(nombre, edad, raza, p2);
-                            break;
-                    }
-                    listaMascotas.add(m1);
+				nuevaMascota(teclado, listaMascotas, p2);
                     break;
 
                 case 3:
@@ -157,5 +96,84 @@ public class ClinicaVeterinaria {
         } while (numero != 5);
 
     }
+
+	public static void nuevaMascota(BufferedReader teclado, ArrayList<Mascota> listaMascotas, Persona p2)
+			throws IOException {
+		//MASCOTA NUEVA
+		System.out.println("AHORA INTRODUCIMOS LA MASCOTA");
+		System.out.println("Introduce el nombre");
+		nombre = teclado.readLine();
+		System.out.println("Introduce la raza");
+		String raza = teclado.readLine();
+		System.out.println("Introduce la edad");
+		edad = 0;
+		try {
+		    edad = Integer.parseInt(teclado.readLine());
+		} catch (Exception e) {
+		    System.err.println("Has causado una excepción porque no has entrado un número entero, gracias");
+		    return;
+		}
+
+		int indiceM = 0;
+		System.out.println("Introduce el tipo de mascota");
+		System.out.println("1. Cerdo");
+		try {
+		    indiceM = Integer.parseInt(teclado.readLine());
+		} catch (IOException e) {
+		    System.err.println("Vaya, un error de entrada/salida... qué raro");
+		} catch (NumberFormatException e) {
+		    System.err.println("Has causado una excepción porque no has entrado un número entero, gracias");
+		    return;
+		}
+
+		Mascota m1;
+		switch (indiceM) {
+		    case 1:
+		        float peso = 0.0f;
+		        System.out.println("Introduce el peso del cerdo o la cerda");
+		        try {
+		            peso = Float.parseFloat(teclado.readLine());
+		        } catch (Exception e) {
+		            System.err.println("Has causado una excepción porque no has entrado un número entero, gracias");
+		            return;
+		        }
+		        m1 = new Cerdo(nombre, edad, raza, p2, peso);
+		        break;
+		    default:
+		        m1 = new Mascota(nombre, edad, raza, p2);
+		        break;
+		}
+		listaMascotas.add(m1);
+	}
+
+	public static void nuevoCliente(BufferedReader teclado, ArrayList<Persona> clientes) throws IOException {
+		//CLIENTE NUEVO
+		System.out.println("INTRODUCIMOS EL CLIENTE");
+		System.out.println("Introduce el nombre");
+		nombre = teclado.readLine();
+		System.out.println("Introduce los apellidos");
+		apellidos = teclado.readLine();
+		System.out.println("Introduce el DNI");
+		dni = teclado.readLine();
+		System.out.println("Introduce la edad");
+		edad = 0;
+		try {
+		    edad = Integer.parseInt(teclado.readLine());
+		} catch (Exception e) {
+		    System.err.println("Has causado una excepción porque no has entrado un número entero, gracias");
+		    return;
+		}
+		Persona p1 = new Persona(nombre, apellidos, dni, edad);
+		clientes.add(p1);
+	}
+
+	public static void mostrarMenu() {
+		System.out.println("¿Qué quieres hacer?");
+		System.out.println("1. Añadir un nuevo cliente");
+		System.out.println("2. Añadir una nueva mascota para un cliente existente");
+		System.out.println("3. Mostrar todos los dueños");
+		System.out.println("4. Mostrar todas las mascotas");
+		System.out.println("5. Salir");
+	}
 
 }
